876404198a4f658695cecd68154f1d4266ab487a
Bakaluvaleria <bakaluvaleria20@gmail.com>
2026-09-17 21:20:06 +0300

    differences

---
+++ Difference.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+
+class MatrixDifferences {
+private:
+    std::vector<std::vector<double>> data;
+    int rows;
+    int cols;
+
+public:
+    // Constructor declaration
+    MatrixDifferences(const std::vector<std::vector<double>>& input_data);
+
+    // Function declarations
+    MatrixDifferences row_difference() const;
+    MatrixDifferences column_difference() const;
+};
